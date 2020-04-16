9d9f6c4bfde19d8fc5a0e5470c4bebb5cbd7296b
J. Cohen <hostelmarketing@gmail.com>
2020-04-15 18:30:59 -0700

    Add different example of grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,27 @@ const IndexPage = () => (
     <Layout>
         <SEO title="Home Page" />
         <h1 className="title is-1">Hello World</h1>
-        <p>Add your home page content here.</p>
+        <div className="columns">
+            <div className="column is-two-thirds">
+                <p className="subtitle">Add your home page content here.</p>
+                <p>
+                    Ipsum similique ab nostrum maxime harum Eum accusantium
+                    numquam possimus autem facilis Totam cupiditate aliquid
+                    illum aperiam recusandae Esse quaerat consectetur distinctio
+                    amet maiores Sapiente saepe error similique reiciendis vel
+                </p>
+                <p>
+                    Sit eveniet veniam numquam natus aliquid! Molestiae velit
+                    dolore labore excepturi quidem? Aut inventore natus illum
+                    vitae blanditiis. Perspiciatis sapiente tempora explicabo
+                    aut provident voluptatem Blanditiis fugit earum corrupti
+                    autem?
+                </p>
+            </div>
+            <div className="column">
+                <img src="https://placekitten.com/300/300" alt="cat" />
+            </div>
+        </div>
         <h2 className="title is-2">Portfolio</h2>
         <div className="columns">
             <div className="column">
