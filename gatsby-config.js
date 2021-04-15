9022d7949972850c12670c7a13a436bb7685df48
J. Cohen <hostelmarketing@gmail.com>
2021-04-15 12:51:33 -0700

    fix the RSS feed and PurgeCSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,8 +65,73 @@ module.exports = {
                             wrapperStyle: `margin-bottom: 1.0725rem`,
                         },
                     },
-                    `gatsby-remark-prismjs`,
                     `gatsby-remark-copy-linked-files`,
+                    {
+                        // Documentation: https://www.gatsbyjs.com/plugins/gatsby-remark-prismjs/
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            // Class prefix for <pre> tags containing syntax highlighting;
+                            // defaults to 'language-' (e.g. <pre class="language-js">).
+                            // If your site loads Prism into the browser at runtime,
+                            // (e.g. for use with libraries like react-live),
+                            // you may use this to prevent Prism from re-processing syntax.
+                            // This is an uncommon use-case though;
+                            // If you're unsure, it's best to use the default value.
+                            classPrefix: "language-",
+                            // This is used to allow setting a language for inline code
+                            // (i.e. single backticks) by creating a separator.
+                            // This separator is a string and will do no white-space
+                            // stripping.
+                            // A suggested value for English speakers is the non-ascii
+                            // character '›'.
+                            inlineCodeMarker: null,
+                            // This lets you set up language aliases.  For example,
+                            // setting this to '{ sh: "bash" }' will let you use
+                            // the language "sh" which will highlight using the
+                            // bash highlighter.
+                            aliases: {},
+                            // This toggles the display of line numbers globally alongside the code.
+                            // To use it, add the following line in gatsby-browser.js
+                            // right after importing the prism color scheme:
+                            //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+                            // Defaults to false.
+                            // If you wish to only show line numbers on certain code blocks,
+                            // leave false and use the {numberLines: true} syntax below
+                            showLineNumbers: false,
+                            // If setting this to true, the parser won't handle and highlight inline
+                            // code used in markdown i.e. single backtick code like `this`.
+                            noInlineHighlight: false,
+                            // This adds a new language definition to Prism or extend an already
+                            // existing language definition. More details on this option can be
+                            // found under the header "Add new language definition or extend an
+                            // existing language" below.
+                            languageExtensions: [
+                                {
+                                    language: "superscript",
+                                    extend: "javascript",
+                                    definition: {
+                                        superscript_types: /(SuperType)/,
+                                    },
+                                    insertBefore: {
+                                        function: {
+                                            superscript_keywords: /(superif|superelse)/,
+                                        },
+                                    },
+                                },
+                            ],
+                            // Customize the prompt used in shell output
+                            // Values below are default
+                            prompt: {
+                                user: "root",
+                                host: "localhost",
+                                global: false,
+                            },
+                            // By default the HTML entities <>&'" are escaped.
+                            // Add additional HTML escapes by providing a mapping
+                            // of HTML entities and their escape value IE: { '}': '&#123;' }
+                            escapeEntities: {},
+                        },
+                    },
                     `gatsby-remark-smartypants`,
                 ],
             },
@@ -144,10 +209,84 @@ module.exports = {
                     "thead",
                     "content",
                 ],
-                whitelistPatternsChildren: [/^token/, /^pre/, /^code/],
-                // develop: true, // Enable while using `gatsby develop`
+                ignore: ["prism.css"], // keeps the code syntax highlighting
+                whitelistPatternsChildren: [/token$/, /^pre/, /^code/],
+                // Enable while using `gatsby develop`
+                // develop: true,
             },
         },
-        `gatsby-plugin-feed`,
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+                    {
+                        site {
+                            siteMetadata {
+                                title
+                                description
+                                siteUrl
+                                site_url: siteUrl
+                            }
+                        }
+                    }
+                `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.edges.map((edge) => {
+                                return Object.assign(
+                                    {},
+                                    edge.node.frontmatter,
+                                    {
+                                        description: edge.node.excerpt,
+                                        date: edge.node.frontmatter.date,
+                                        url:
+                                            site.siteMetadata.siteUrl +
+                                            edge.node.fields.slug,
+                                        guid:
+                                            site.siteMetadata.siteUrl +
+                                            edge.node.fields.slug,
+                                        custom_elements: [
+                                            {
+                                                "content:encoded":
+                                                    edge.node.html,
+                                            },
+                                        ],
+                                    }
+                                );
+                            });
+                        },
+                        query: `
+                            {
+                                allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+                                    edges {
+                                        node {
+                                            excerpt
+                                            html
+                                            fields {
+                                                slug
+                                            }
+                                            frontmatter {
+                                                title
+                                                date
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        `,
+                        output: "/rss.xml",
+                        title: `${TITLE} RSS Feed`,
+                        // optional configuration to insert feed reference in pages:
+                        // if `string` is used, it will be used to create RegExp and then test if pathname of
+                        // current page satisfied this regular expression;
+                        // if not provided or `undefined`, all pages will have feed reference inserted
+                        match: "^/blog/",
+                        // optional configuration to specify external rss feed, such as feedburner
+                        // link: "https://feeds.feedburner.com/gatsby/blog",
+                    },
+                ],
+            },
+        },
     ],
 };
